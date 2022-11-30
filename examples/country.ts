c7bb531d89d37952f986f5c7a1b8fe4955615a7c
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-11-30 19:27:25 +0900

    impl simplest

---
+++ examples/country.ts
@@ -0,0 +1,14 @@
+type Country = {
+  Jp: Human,
+  En: Human,
+}
+
+type Human = {
+  name: Name,
+  age: number
+}
+
+type Name = {
+  first: string
+  last: string
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,43 @@
-function main() {
-  return "Hello"
+import * as ts from "typescript";
+import {Project, Type } from "ts-morph";
+import * as process from "process";
+
+
+const path = './examples/country.ts'
+
+function main6() {
+  const project = new Project({})
+  project.addSourceFileAtPath(path)
+  const sourceFile = project.getSourceFile(path)
+  if (!sourceFile)
+    process.exit(1)
+  const checker = project.getTypeChecker();
+  const typeAlias = sourceFile.getTypeAliasOrThrow("Country")
+
+  const visitType = (name: string, entryType: Type<ts.Type>, result: any) => {
+    for (const symbol of entryType.getProperties()) {
+      const name = symbol.getName()
+      const type = symbol.getTypeAtLocation(typeAlias)
+      if (type.isArray()) {
+        continue
+      }
+      if (type.isNumber()) {
+        result[name] = 10
+        continue
+      }
+      if (type.isString()) {
+        result[name] = "fuga"
+        continue
+      }
+      if (type.isObject()) {
+        result[name] = visitType(name, type, {})
+      }
+    }
+    return result
+  }
+
+  const result = visitType("Country", typeAlias.getType(), {})
+  console.log(JSON.stringify(result, null, 2))
 }
 
-main()
+main6()
